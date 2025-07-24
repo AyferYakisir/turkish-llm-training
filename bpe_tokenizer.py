499c5ea903f0115fc0ebc8f31170a9f2cda2d6e6
Ayfer Yakışır <yakisirayfer@gmail.com.com>
2025-07-24 14:03:18 +0300

    SentencePieceProcessor kütüphanesi ile eğitilen model ile text1 tokenleri alındı.

--- bpe_tokenizer.py
+++ bpe_tokenizer.py
@@ -1,4 +1,5 @@
 import sentencepiece as spm
+from sozlukleTokenize import text1
 
 spm.SentencePieceTrainer.Train(
     input="text.txt",
@@ -6,3 +7,10 @@ spm.SentencePieceTrainer.Train(
     vocab_size=49,
     model_type="bpe",
 )
+
+spm_tokenizer = spm.SentencePieceProcessor(model_file="spm_tokenizer.model")
+spm_ids = spm_tokenizer.Encode("text1")
+print("Token ID'leri:", spm_ids)
+spm_tokens = spm_tokenizer.Encode(text1, out_type=str)
+print("Token Stringleri:", spm_tokens)
+spm_ids, spm_tokens
