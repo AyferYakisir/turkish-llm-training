cef95a32828f266338747e05e5d517cfbe57bc7c
Ayfer Yakışır <yakisirayfer@gmail.com.com>
2025-07-24 18:48:37 +0300

    tokenizer.json dosyası içersinden birkaç kelimeye 4  boyutlu embedding vektörleri tanımlandı.

---
+++ embedding.py
@@ -0,0 +1,11 @@
+embed = {
+  "Yarın": [0.1, 0.0, 0.0, 0.0],
+  "sabah": [0.4, 0.2, 0.04, 0.3],
+  "güneş": [0.0, 0.24, 0.02, 0.01],
+  "doğarken": [0.0, 0.0, 0.03, 0.01],
+  "kuşlar": [0.0, 0.0, 0.0, 0.0],
+  "neşeyle": [0.2, 0.0, 0.1, 0.5],
+  "ötüyor": [0.3, 0.1, 0.0, 0.2]
+}
+
+print(embed["Yarın"], embed["sabah"], embed["güneş"], embed["doğarken"], embed["kuşlar"], embed["neşeyle"], embed["ötüyor"])
