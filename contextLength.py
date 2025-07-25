3d8d56edd14d590ae9caf0d96c2027997c99e9d5
Ayfer Yakışır <yakisirayfer@gmail.com.com>
2025-07-25 13:35:13 +0300

    token uzunluğu sağlandı.

---
+++ contextLength.py
@@ -0,0 +1,17 @@
+from tokenizer import Tokenizer
+prompt = "Gökyüzünde bulutlar usulca süzülüyor, "
+
+next_token = " "
+
+output = " hava taze ve ferah."
+data = "Gökyüzünde bulutlar <pad> <pad> "
+hedef = "hava taze <pad> <pad> "
+ 
+context_length = 7
+
+
+tokenizer = Tokenizer("tokenizer.json")
+
+ids = tokenizer.encode(prompt)
+
+print(len(ids), ids)
