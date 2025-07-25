8cbb3a6e8f8d48287216f6ae8a551475ad4ee436
Ayfer Yakışır <yakisirayfer@gmail.com.com>
2025-07-25 14:09:36 +0300

    Model cümledeki kelimelerin sırasını öğrenebilmesi için kelimenin pozisyon bilgisini alma işlemi yapıldı.

--- positional_embedding.py
+++ positional_embedding.py
@@ -17,3 +17,13 @@ print(tokens.shape)
 
 meanings = embeddings(tokens)
 print(meanings)
+
+torch.manual_seed(1)
+pos_embeddings = torch.nn.Embedding(num_embeddings=context_length, embedding_dim=4)
+
+pos_meanings = pos_embeddings(torch.tensor([i for i in range(tokens.shape[0])]))
+
+print(pos_meanings.shape)
+
+meanings_in_context = meanings + pos_meanings
+print(meanings_in_context.shape)
