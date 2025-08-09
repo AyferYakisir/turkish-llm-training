08f9efb0b6ede2d0614853f69345e1c4ae2529fc
Ayfer Yakışır <yakisirayfer@gmail.com.com>
2025-08-09 15:02:16 +0300

    Gökyüzü ve bulut vektörleri için dot product ve bileşen bazlı çarpım hesaplamaları eklendi

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -7,4 +7,13 @@ dot_product = (
     gokyuzunde_pos[2] * bulutlar_pos[2] +
     gokyuzunde_pos[3] * bulutlar_pos[3]
 )
-print(dot_product)
+print(dot_product) 
+
+cos_sim_uzakligi = gokyuzunde_pos[0] * bulutlar_pos[0]
+cos_sim_parlakligi = gokyuzunde_pos[1] * bulutlar_pos[1]
+cos_sim_kizilligi = gokyuzunde_pos[2] * bulutlar_pos[2]
+cos_sim_maviligi = gokyuzunde_pos[3] * bulutlar_pos[3]
+
+total_cos_sim = cos_sim_uzakligi + cos_sim_parlakligi + cos_sim_kizilligi + cos_sim_maviligi
+
+print(cos_sim_uzakligi, cos_sim_parlakligi, cos_sim_kizilligi, cos_sim_maviligi, total_cos_sim)
